b473808f86fe32f5bed169706bfceab78281a6f0
leTabs <125308756+leTabs@users.noreply.github.com>
2024-04-02 21:17:08 +0300

    Create index.js

---
+++ functionality/index.js
@@ -0,0 +1,7 @@
+const homeViewCvBtn = document.querySelector('.home-view-cv-btn')
+
+
+const alertsArray = [homeViewCvBtn]
+homeViewCvBtn.addEventListener('click', ()=>{
+    alert('comming soon')
+})
