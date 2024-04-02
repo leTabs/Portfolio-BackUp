0eb8daab95dffb1c07a5eeb143ea7fbea6607901
leTabs <125308756+leTabs@users.noreply.github.com>
2024-04-02 23:34:50 +0300

    Update index.js

--- functionality/index.js
+++ functionality/index.js
@@ -1,7 +1,30 @@
 const homeViewCvBtn = document.querySelector('.home-view-cv-btn')
+// nav elements
+const navHome = document.querySelector('.nav-home')
+const navMyWork = document.querySelector('.nav-my-work')
+const navSkills = document.querySelector('.nav-skills')
+const navCommunication = document.querySelector('.nav-communication')
+
+// sections
+const sectionHome = document.querySelector('.section-home')
+const sectionMyWork = document.querySelector('.section-my-work')
+const sectionSkills = document.querySelector('.section-skills')
+
+const alertsArray = [homeViewCvBtn, navCommunication]
+
+for(let i = 0; i < alertsArray.length; i++){
+    alertsArray[i].addEventListener('click', ()=>{
+        alert('comming soon')
+    })
+}
 
 
-const alertsArray = [homeViewCvBtn]
-homeViewCvBtn.addEventListener('click', ()=>{
-    alert('comming soon')
+navHome.addEventListener('click', ()=>{
+    sectionHome.scrollIntoView()
+})
+navMyWork.addEventListener('click', ()=>{
+    sectionMyWork.scrollIntoView()
+})
+navSkills.addEventListener('click', ()=>{
+    sectionSkills.scrollIntoView()
 })
