871cfaf7e1d7456817905d79b60f8e1f1d43b87e
leTabs <125308756+leTabs@users.noreply.github.com>
2024-04-04 22:21:05 +0300

    Update index.js

--- functionality/index.js
+++ functionality/index.js
@@ -13,6 +13,14 @@ const sectionCommunication = document.querySelector('.section-communication')
 
 // contact elements
 const contactCertifications = document.querySelector('.contact-certifications')
+const certificationsContainer = document.querySelector('.certifications-container')
+const certificationsCloseBtn = document.querySelector('.certifications-close-btn')
+const certificateContainer = document.querySelector('.certificate-container')
+const cpi01 = document.querySelector('.cpi-01')
+const cpi02 = document.querySelector('.cpi-02')
+const cpi03 = document.querySelector('.cpi-03')
+
+// 
 const alertsArray = [homeViewCvBtn]
 
 for(let i = 0; i < alertsArray.length; i++){
@@ -22,6 +30,7 @@ for(let i = 0; i < alertsArray.length; i++){
 }
 
 
+// scroll in to view functionalities
 navHome.addEventListener('click', ()=>{
     sectionHome.scrollIntoView()
 })
@@ -33,4 +42,37 @@ navSkills.addEventListener('click', ()=>{
 })
 navCommunication.addEventListener('click', ()=>{
     sectionCommunication.scrollIntoView()
+})
+
+
+// contact (animation & opening/closing funcionality)
+contactCertifications.addEventListener('click', ()=>{
+    certificationsContainer.style.opacity = '0'
+    certificationsContainer.style.display = 'block'
+    setTimeout(()=>{
+    certificationsContainer.style.opacity = '1'
+    }, 1)
+})
+
+certificationsCloseBtn.addEventListener('click', ()=>{
+    certificationsContainer.style.top = '55%'
+    certificationsContainer.style.opacity = '0'
+    setTimeout(()=>{
+        certificationsContainer.style.top = '50%'
+        certificationsContainer.style.display = 'none'
+    }, 250)
+})
+// 
+cpi01.addEventListener('click', ()=>{alert('cpi1')})
+cpi02.addEventListener('click', ()=>{alert('cpi2')})
+
+// certificate opening
+cpi03.addEventListener('click', ()=>{
+    certificateContainer.style.display = 'block';
+    // just change the src value of the image + add a switch for knowing when to display and when to close
+    setTimeout(()=>{
+        certificateContainer.style.width = '90%'
+        certificateContainer.style.height = '40vh'
+        certificateContainer.style.marginTop = '2rem'
+    }, 1)
 })
