3d45a5e079556173d4314b9f49e34e5e83066767
leTabs <125308756+leTabs@users.noreply.github.com>
2024-04-11 22:10:29 +0300

    Update vanue-space-template.js

--- functionality/vanue-space-template.js
+++ functionality/vanue-space-template.js
@@ -1,15 +1,19 @@
 // sections
 const sectionGeneralDescription = document.querySelector('.section-general-desc')
 const templateDisplay = document.querySelector('.template-display')
+const sectionTemplateAllImgs = document.querySelector('.template-all-imgs')
 
 // nav elements
 const navHome = document.querySelector('.nav-home')
 const navOverview = document.querySelector('.nav-overview')
-
+const navGallery = document.querySelector('.nav-gallery')
 
 navOverview.addEventListener('click', ()=>{
     templateDisplay.scrollIntoView()
 })
 navHome.addEventListener('click', ()=>{
     sectionGeneralDescription.scrollIntoView()
+})
+navGallery.addEventListener('click', ()=>{
+    sectionTemplateAllImgs.scrollIntoView()
 })
