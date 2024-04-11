37d801953d7553a1145a55bf0e9d4d64891f1c7e
leTabs <125308756+leTabs@users.noreply.github.com>
2024-04-12 00:09:09 +0300

    Update vanue-space-template.js

--- functionality/vanue-space-template.js
+++ functionality/vanue-space-template.js
@@ -8,6 +8,11 @@ const navHome = document.querySelector('.nav-home')
 const navOverview = document.querySelector('.nav-overview')
 const navGallery = document.querySelector('.nav-gallery')
 
+// image viewing elements
+const expandHideBtnArray = document.querySelectorAll('.expand-hide-icon')
+const imgFrameArray = document.querySelectorAll('.img-container')
+const templateImgArray = document.querySelectorAll('.template-img')
+
 navOverview.addEventListener('click', ()=>{
     templateDisplay.scrollIntoView()
 })
@@ -17,3 +22,14 @@ navHome.addEventListener('click', ()=>{
 navGallery.addEventListener('click', ()=>{
     sectionTemplateAllImgs.scrollIntoView()
 })
+
+for(let i = 0; i < expandHideBtnArray.length; i++){
+    expandHideBtnArray[i].addEventListener('click', ()=>{
+        imgFrameArray[i].style.height = '70vh'
+        imgFrameArray[i].style.padding = '2rem 2rem'
+        
+        setTimeout(()=>{
+            templateImgArray[i].style.display = 'inline'
+        }, 250)
+    })
+}
